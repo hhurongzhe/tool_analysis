0261f2f97f0f077da043c74b1c445f782ffaadd5
hhurongzhe <rongzhe_hu@pku.edu.cn>
2026-01-26 18:34:12 +0800

    add auto_blocking analysis

--- lib/analyze.py
+++ lib/analyze.py
@@ -46,3 +46,31 @@ def blocking_fixed(data: np.array, L: int, pos=0):
     std_err_err = std_err / np.sqrt(2 * (num_blocks - 1))
 
     return mean, std_err, std_err_err
+
+
+# blocking analysis with automatic block size selection
+def blocking_auto(data: np.array, pos=0):
+    if pos < 0 or pos >= 1:
+        print("Warning: position pos is out of range. Using pos=0 instead.")
+        pos = 0
+    data_list = data[int(pos * len(data)) :]
+    l_max = int(np.log2(len(data_list)))
+    l_list = np.array([2**n for n in range(1, l_max)])
+    mean_list = []
+    std_list = []
+    std_std_list = []
+    for L in l_list:
+        mean, std, std_std = blocking_fixed(data_list, L)
+        mean_list.append(mean)
+        std_list.append(std)
+        std_std_list.append(std_std)
+    mean_list = np.array(mean_list)
+    std_list = np.array(std_list)
+    std_std_list = np.array(std_std_list)
+    decreasing_idx = np.where(np.diff(std_list) < 0)[0]
+    if len(decreasing_idx) == 0:
+        print("Warning: mean_list is monotonically increasing. Returning values at the end.")
+        idx = len(l_list) - 1
+    else:
+        idx = decreasing_idx[0]
+    return int(l_list[idx]), float(mean_list[idx]), float(std_list[idx]), float(std_std_list[idx])
--- tool/git.py
+++ tool/git.py
@@ -1,7 +1,7 @@
 import subprocess
 
 # * This script is used to commit changes to a git repository.
-message = "small update."
+message = "add auto_blocking analysis"
 
 
 command1 = "git add ."
